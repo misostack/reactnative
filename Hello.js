69b5f8905d82d6bbadd3e2fc37f598de490140d7
misostack <misostack.com@gmail.com>
2020-02-25 18:41:58 +0700

    Add config

--- Hello.js
+++ Hello.js
@@ -1,17 +1,70 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
+import Pusher from 'pusher-js/react-native';
+
+Pusher.logToConsole = true;
+
+import { of, BehaviorSubject  } from 'rxjs'
+
+import { mergeMap, delay, takeUntil } from 'rxjs/operators';
+import Config from './config.local';
+
+const socket = new Pusher(Config.PUSHER_API_KEY, Config.PUSHER_OPTIONS);
+
+const channel = socket.subscribe('my-channel');
+
+
 export default class Hello extends Component {
+
+	message$ = new BehaviorSubject("")
+
 	constructor(props) {
 		super(props)
 		this.state = {
 			data: 'Hello world'
 		}
+
 	}
+
+  componentDidMount() {
+  	try{
+			
+  		this.message$.pipe(delay(1000)).subscribe(value => {
+				console.log(value)
+				this.setState({
+					data: this.state.data + '\n' + value
+				})  			
+  		})
+
+			of(1, 2, 3, 'Hello', 'World')
+			.pipe(delay(1000))
+			.subscribe(value => {
+				this.message$.next(Date.now().toString() + ':' + value)
+			})  		
+	 //  	console.log('abc')
+		// 	console.log('channel', channel)
+
+			channel.bind('my-event',
+			    (data) => {
+			        this.message$.next(Date.now().toString() + ':' + JSON.stringify(data))
+			    }
+			);
+
+		}catch(err){
+			console.error(err)
+		}
+  }
+
+  componentWillUnmount() {
+
+  }  
+
 
   render() {  	
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      	<Text>Chat View</Text>
         <Text>{this.state.data}</Text>
       </View>
     );
---
+++ config.js
@@ -0,0 +1,9 @@
+const Config = {
+	PUSHER_API_KEY: '',
+	PUSHER_OPTIONS: {
+	  cluster: '',
+	  forceTLS: true		
+	},
+}
+
+export default Config;
